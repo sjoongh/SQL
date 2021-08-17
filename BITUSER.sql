2eb07a7e65750b1f1e7756d3036227fa9a4f7db2
sjoongho <az45687@naver.com>
2021-08-17 16:30:17 +0900

    08-17

--- BITUSER.sql
+++ BITUSER.sql
@@ -14,3 +14,19 @@ VALUES(seq_emaillist_pk.NEXTVAL, '중호', '신', 'az45687@naver.com');
 SELECT * FROM emaillist;
 
 COMMIT;
+
+CREATE TABLE users (
+    no number primary key,
+    name varchar2(20) NOT NULL,
+    password varchar2(20) NOT NULL,
+    email varchar2(128) NOT NULL UNIQUE,
+    gender char(1) CHECK(gender IN ('M', 'F')),
+    created_at date DEFAULT sysdate);
+CREATE SEQUENCE seq_users_pk
+    START WITH 1
+    INCREMENT BY 1;
+INSERT INTO users (no, name, password, email, gender)
+VALUES(seq_users_pk.nextval, '관리자', '12345678', 'admin@example.com', 'M');
+commit;
+
+SELECT * FROM users;
