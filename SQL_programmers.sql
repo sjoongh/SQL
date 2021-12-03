e8084d6473a02e369467bd842c46e22e88886112
pansu <77228818+sjoongh@users.noreply.github.com>
2021-12-03 21:50:14 +0900

    Update SQL_programmers.sql
    Mission 25 중성화 여부 파악하기

--- SQL_programmers.sql
+++ SQL_programmers.sql
@@ -140,3 +140,13 @@ from emp
 where job=(select job from emp where empno=7521) 
 and 
 sal>(select sal from emp where empno=7934);
+
+--Mission 25
+SELECT ANIMAL_ID, NAME, 
+(CASE 
+ WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+ WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+ ELSE 'X'
+ END) AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
