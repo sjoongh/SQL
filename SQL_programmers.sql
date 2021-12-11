11f2d2de237707611f63fa2e0ec84aa40b5569e4
pansu <77228818+sjoongh@users.noreply.github.com>
2021-12-11 21:06:30 +0900

    Deptno구하기

--- SQL_programmers.sql
+++ SQL_programmers.sql
@@ -157,3 +157,15 @@ SELECT B.ANIMAL_ID, B.NAME
 FROM ANIMAL_INS A, ANIMAL_OUTS B
 WHERE A.ANIMAL_ID = B.ANIMAL_ID AND A.DATETIME > B.DATETIME
 ORDER BY A.DATETIME;
+
+--Mission 27
+select deptno, count(*)
+from emp
+group by deptno
+having count(deptno) =
+(select max(count(*))
+from emp
+group by deptno)
+
+
+
