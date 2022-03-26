e070b1db6b8f1f3fa57fc8f7959d529b7d13e544
pansu <77228818+sjoongh@users.noreply.github.com>
2022-03-26 20:32:52 +0900

    프로그래머스 date

--- JustTest.sql
+++ JustTest.sql
@@ -14,3 +14,12 @@ UNION
 SELECT COL1, COL2
 FROM TBL1)
 GROUP BY COL1, COL2；
+
+-- DATETIME에서 DATE로 형 변환ANIMAL_INS 
+-- 테이블에 등록된 모든 레코드에 대해, 
+-- 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 
+-- SQL문을 작성해주세요. 이때 결과는 아이디 순으로 조회
+-- 해야 합니다.
+SELECT ANIMAL_ID, NAME, 
+DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜 
+FROM ANIMAL_INS
